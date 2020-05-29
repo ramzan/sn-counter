e19d5f6208c67408e26b32c578d6c4115eac27cf
Ramzan <ramzan.sheikh@mail.utoronto.ca>
2020-05-29 18:59:25 -0400

    Updates

---
+++ precache-manifest.4f02ca1d055f05426b9c8bc1287a7f8d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ca3df72dcf8d8b2e9c0a691f2fa4635",
+    "url": "/sn-counter/index.html"
+  },
+  {
+    "revision": "464606e6f954ff024ffc",
+    "url": "/sn-counter/static/css/main.0dcb7fdb.chunk.css"
+  },
+  {
+    "revision": "80835c280df9ec153099",
+    "url": "/sn-counter/static/js/2.9b52fe5f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/sn-counter/static/js/2.9b52fe5f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "464606e6f954ff024ffc",
+    "url": "/sn-counter/static/js/main.90dfe230.chunk.js"
+  },
+  {
+    "revision": "8ad7da597fcb7baf4f89",
+    "url": "/sn-counter/static/js/runtime-main.0db8caea.js"
+  },
+  {
+    "revision": "1533b4c231e9c3224dc790b68530c741",
+    "url": "/sn-counter/static/media/kebab.1533b4c2.svg"
+  },
+  {
+    "revision": "f3735be92b3aa21a593d6fbbe8cf67d6",
+    "url": "/sn-counter/static/media/x.f3735be9.svg"
+  }
+]);
